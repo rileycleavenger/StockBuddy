026cbb393ec7a8942282a030ece76517e5172263
piper-jones <139887562+piper-jones@users.noreply.github.com>
2024-05-18 10:26:49 -0400

    Nav bar first draft

--- my-ai-stock-buddy/src/components/Nav/Nav.tsx
+++ my-ai-stock-buddy/src/components/Nav/Nav.tsx
@@ -1,8 +1,39 @@
 import React, {FC} from 'react';
+import styles from './Nav.module.css';
+
+
+
 
 const Nav: FC = () => {
     return (
-        <div>Nav</div>
+        <nav className={styles.navbar}>
+
+            <div className={styles['logo-container']}>
+                <a href='https://www.websiteName.com'>Logo??</a>
+            </div>
+
+            <div className={styles['links-container']}>
+
+
+                <div className={styles['About']}>
+                <a href='/about'>About</a>
+                </div>
+
+                <div className={styles['Login']}>
+                    <a href='/login'>Login</a>
+                </div>
+
+                <div className={styles['SignUp']}>
+                    <a href='/signup'>Sign Up</a>
+                </div>
+
+                <div className={styles['Profile']}>
+                    <a href='/profile'>Profile</a>
+                </div>
+
+
+            </div>
+        </nav>
     );
 }
 
