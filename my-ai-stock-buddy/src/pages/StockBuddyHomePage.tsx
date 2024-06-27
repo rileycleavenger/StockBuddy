ccdf519261a16ef3d3022c8c1cc1a77ef89a37a7
piper-jones <139887562+piper-jones@users.noreply.github.com>
2024-06-26 21:01:30 -0400

    The rest of main UI design with some functionality added

--- my-ai-stock-buddy/src/pages/StockBuddyHomePage.tsx
+++ my-ai-stock-buddy/src/pages/StockBuddyHomePage.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from '../components/Nav/Nav';
 import styles from './StockBuddyHomePage.module.css';
 
 
+const filters = Array.from({ length: 8 }, (_, i) => `Filter ${i + 1}`);
+
 const StockBuddyHomePage: React.FC = () => {
+
+
+/* Filter color change and labels handling: */
+const [clicked, setClicked] = useState<boolean[]>(Array(filters.length).fill(false));
+
+const handleFilterClick = (index: number) => {
+    setClicked(prevState => {
+        const newClicked = [...prevState];
+        newClicked[index] = !newClicked[index];
+        return newClicked;
+    });
+};
+
+
+    /* Switch movement handling: */
+    const [isRightAligned, setIsRightAligned] = useState(true);
+
+    const toggleSwitch = () => {
+      setIsRightAligned(!isRightAligned);
+    };
+
     return (
       <div className={styles.body}>
         <Nav />
@@ -11,7 +34,48 @@ const StockBuddyHomePage: React.FC = () => {
         <div className={styles.content}>
 
           <div className={styles.main}>
-            <p>This is where the main content goes</p>
+            <div className={styles.leftColumn}>
+              <div className = {styles.switch} 
+              style={{
+                justifyContent: isRightAligned ? 'flex-end' : 'flex-start',
+              }}              
+              >
+              <div className = {styles.switchButton} onClick={toggleSwitch}>
+                <p>Click me</p>
+              </div>
+              </div>
+
+
+            <div className={styles.filterContainer}>
+            {filters.map((filter, index) => (
+                  <div
+                    key={index}
+                    className={`${styles.filter} ${clicked[index] ? styles.clicked : ''}`}
+                    onClick={() => handleFilterClick(index)}
+                  > {/* Replace with your icon */}
+                    {filter}
+                  </div>
+                ))}
+
+    
+        </div>
+        </div>
+
+        <div className={styles.rightColumn}>
+        <div className = {styles.contentBox}></div>
+
+        <div className = {styles.redButtonRow}>
+        <div className = {styles.generateButton}>
+          <p>Generate</p>
+        </div>
+        <div className = {styles.saveResultsButton}>
+          <p>Save Results</p>
+        </div>
+        </div>
+
+        
+
+          </div>
           </div>
 
 
@@ -33,6 +97,9 @@ const StockBuddyHomePage: React.FC = () => {
 
 
         </div>
+
+        
+        
 
       </div>
     );
