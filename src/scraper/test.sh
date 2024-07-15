3076a338ccd467624e7bd2239b883fa6066791a7
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-07-14 21:44:34 -0700

    fixed service name

--- src/scraper/test.sh
+++ src/scraper/test.sh
@@ -5,4 +5,4 @@ gcloud artifacts repositories create sb-repo --repository-format=docker --locati
 gcloud auth configure-docker us-central1-docker.pkg.dev
 docker build --no-cache -t us-central1-docker.pkg.dev/gator-path-tracker/sb-repo/test-sb .
 docker push us-central1-docker.pkg.dev/gator-path-tracker/sb-repo/test-sb
-gcloud run deploy soc-service --image us-central1-docker.pkg.dev/gator-path-tracker/sb-repo/test-sb --region us-central1 --platform managed --memory=32Gi --cpu 8 --timeout=60m
+gcloud run deploy sb-service --image us-central1-docker.pkg.dev/gator-path-tracker/sb-repo/test-sb --region us-central1 --platform managed --memory=32Gi --cpu 8 --timeout=60m
