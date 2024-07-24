d57fc0beaab31d4029209de32f1546f5529b61ed
Dravani <42421220+Dravani@users.noreply.github.com>
2024-07-24 14:57:35 -0400

    fixed sign in page

--- my-ai-stock-buddy/src/pages/SignUpPage.tsx
+++ my-ai-stock-buddy/src/pages/SignUpPage.tsx
@@ -11,25 +11,25 @@ const SignUpPage: React.FC = () => {
       <div className="signup-box">
         <h2 className="signup-title">Create an account</h2>
         <form className="signup-form">
-          <div className="form-group">
+          <div className="form-group-sign">
             <label htmlFor="email">Enter Email</label>
             <input type="email" id="email" name="email" placeholder="email@domain.com" required />
           </div>
-          <div className="form-group">
+          <div className="form-group-sign">
             <label htmlFor="password">Enter Password</label>
             <input type="password" id="password" name="password" placeholder="●●●●●●●●" required />
           </div>
-          <div className="form-group">
+          <div className="form-group-sign">
             <label htmlFor="confirm-password">Confirm Password</label>
             <input type="password" id="confirm-password" name="confirm-password" placeholder="●●●●●●●●" required />
           </div>
-          <button type="submit" className="signup-button">Sign up with email</button>
+          <button type="submit" className="signup-button-sign">Sign up with email</button>
         </form>
         <div className="login-link">
           <span>Already have an account?</span>
         </div>
         <Link to="/login">
-          <button className="login-button">Login</button>
+          <button className="login-button-sign">Login</button>
         </Link>
       </div>
     </div>
